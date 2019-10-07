5b58bd3084d4b18de29e1cc86922dfc18e875732
nidhi07arvind <49506508+nidhi07arvind@users.noreply.github.com>
2019-10-06 21:38:07 -0700

    backend api for status update and search views

--- backend/routes/buyers.js
+++ backend/routes/buyers.js
@@ -81,6 +81,45 @@ app.post("/restaurant-display", function(req, res) {
   }
 });
 
+app.post("/cuisine-display", function(req, res) {
+  console.log("Inside Restaurant Details POST!");
+  console.log(req.body);
+
+  if (req.session.user) {
+    pool.getConnection(function(err, conn) {
+      if (err) {
+        console.log("Error in establishing connection");
+        res.writeHead(400, {
+          "Content-type": "text/plain"
+        });
+        res.end("Error in establishing connection");
+      } else {
+        console.log("Connection Established");
+        console.log("Request body:", req.body.cuisine);
+        const cuisine = req.body.cuisine;
+
+        // var sql =
+        //   "SELECT * from item where cuisine =" + mysql.escape(req.body.cuisine);
+        conn.query(
+          "SELECT t1.*,t2.* FROM item t1, restaurant t2 where t1.res_id=t2.res_id AND t1.cuisine LIKE?",
+          "%" + cuisine + "%",
+          function(err, result) {
+            if (err) {
+              console.log("Error in searching items!");
+              res.json({ data: "Error in gettings items" });
+            } else {
+              console.log("Items loaded successfully");
+              console.log(result);
+              console.log(JSON.stringify(result));
+              res.status(200).send(JSON.stringify(result));
+            }
+          }
+        );
+      }
+    });
+  }
+});
+
 app.get("/breakfast-details", function(req, res) {
   console.log("Inside Breakfast Details GET!");
   const userSession = req.session.user;
@@ -136,6 +175,108 @@ app.get("/breakfast-details", function(req, res) {
               res.send(200, JSON.stringify(result));
             }
           });
+        }
+      });
+    }
+  });
+});
+
+app.post("/addtocart", function(req, res) {
+  console.log("Inside addtocart POST");
+  console.log("Request Body: ", req.body);
+  //console.log("Request Body: ", req.body.item_id);
+  const userSession = req.session.user;
+
+  if (req.session.user) {
+    pool.getConnection(function(err, conn) {
+      if (err) {
+        console.log("Error in establishing connection!");
+        res.writeHead(400, {
+          "Content-type": "text/plain"
+        });
+        res.end("Error in establishing connection!");
+      } else {
+        const order_id = Math.floor(Math.random() * 10000);
+        const quantity = 1;
+        const item_id = req.body.item_id;
+        // console.log("Item_ID: ", item_id);
+        var sql =
+          "INSERT into cart (item_id,res_id,email,item_name,price) VALUES( " +
+          mysql.escape(req.body.item_id) +
+          "," +
+          mysql.escape(req.body.res_id) +
+          "," +
+          mysql.escape(req.session.user.email) +
+          "," +
+          mysql.escape(req.body.item_name) +
+          "," +
+          mysql.escape(req.body.price) +
+          ");";
+        // var sql =
+        //   "INSERT into cart (item_id,res_id,email,item_name,quantity,price) VALUES(" +
+        //   mysql.escape(req.body.item_id) +
+        //   "," +
+        //   mysql.escape(req.body.res_id) +
+        //   "," +
+        //   mysql.escape(req.session.user.email) +
+        //   "," +
+        //   mysql.escape(req.body.item_name) +
+        //   "," +
+        //   mysql.escape(quantity) +
+        //   "," +
+        //   mysql.escape(req.body.price) +
+        //   ");";
+
+        conn.query(sql, function(err, result) {
+          if (err) {
+            console.log("Error in adding item to cart");
+            res.writeHead(400, {
+              "Content-type": "text/plain"
+            });
+            res.end("Error in adding item to cart");
+          } else {
+            console.log("Adding item to cart successful!");
+            res.writeHead(200, {
+              "Content-type": "text/plain"
+            });
+            res.end("Adding a item to cart successful!");
+          }
+        });
+      }
+    });
+  }
+});
+
+app.get("/cart-details", function(req, res) {
+  console.log("Inside Cart Details GET!");
+  const userSession = req.session.user;
+  console.log(req.session.user);
+
+  //if (req.session.user) {
+  pool.getConnection(function(err, conn) {
+    if (err) {
+      console.log("Error in establishing connection");
+      res.writeHead(400, {
+        "Content-type": "text/plain"
+      });
+      res.end("Error in establishing connection");
+    } else {
+      console.log("Established connection");
+
+      var sql =
+        "SELECT * from cart WHERE email= " + mysql.escape(userSession.email);
+
+      conn.query(sql, function(err, result) {
+        if (err) {
+          console.log("Error in getting item details");
+
+          res.json({ data: "Error in getting cart details" });
+        } else {
+          console.log("Cart loaded successfully");
+          console.log(result);
+          console.log(JSON.stringify(result));
+
+          res.send(JSON.stringify(result));
         }
       });
     }
@@ -207,6 +348,91 @@ app.get("/past-order", function(req, res) {
           res.send(JSON.stringify(result));
         }
       });
+    }
+  });
+});
+
+app.post("/place-order", function(req, res) {
+  console.log("Inside Place Order GET");
+  console.log("Request Body:", req.body);
+  console.log("Res_id:", req.body[0].res_id);
+  console.log("Req Length: ", req.body.length);
+
+  const order_id = Math.floor(Math.random() * 1000);
+  const res_id = req.body[0].res_id;
+  const email = req.body[0].email;
+  // const order_id = 101;
+  // const res_id = 1;
+  // const email = "nidhi@gmail.com";
+  pool.getConnection(function(err, conn) {
+    if (err) {
+      console.log("Error in establishing connection!");
+      res.writeHead(400, {
+        "Content-type": "text/plain"
+      });
+      res.end("Error in establishing connection!");
+    } else {
+      var sql2 =
+        "INSERT into orders (order_id,res_id,email) VALUES(" +
+        mysql.escape(order_id) +
+        "," +
+        mysql.escape(res_id) +
+        "," +
+        mysql.escape(email) +
+        ");";
+
+      conn.query(sql2, function(err, result) {
+        if (err) {
+          console.log("Error in adding order");
+          res.writeHead(400, {
+            "Content-type": "text/plain"
+          });
+          res.end("Error in adding an order");
+        } else {
+          console.log("Adding order successful!");
+          // res.writeHead(200, {
+          //   "Content-type": "text/plain"
+          // });
+          res.end("Adding order successful!");
+        }
+      });
+
+      for (var index = 0; index < req.body.length; index++) {
+        var sql3 =
+          "INSERT INTO owner_to_item (order_id,item_id) VALUES(" +
+          mysql.escape(order_id) +
+          "," +
+          mysql.escape(req.body[index].item_id) +
+          ");";
+
+        conn.query(sql3, function(err, result) {
+          if (err) {
+            console.log("Error in adding order");
+            // res.writeHead(400, {
+            //   "Content-type": "text/plain"
+            // });
+            // res.json({ data: "Error in getting items" });
+          } else {
+            console.log("Order placed successfully!", index);
+            // res.writeHead(200, {
+            //   "Content-type": "text/plain"
+            // });
+            res.end("Order placed successfully!");
+            //res.status(200).send(JSON.stringify(result));
+          }
+        });
+
+        var sql4 = "DELETE from CART where email= " + mysql.escape(email);
+        conn.query(sql4, function(err, result) {
+          if (err) {
+            console.log("Error in deleting items from cart");
+          } else {
+            console.log("Cart cleared", index);
+            res.end("Cart cleared successfully!");
+            //res.status(200).send(JSON.stringify(result));
+          }
+        });
+      }
     }
   });
 });
--- backend/routes/owners.js
+++ backend/routes/owners.js
@@ -137,9 +137,8 @@ app.get("/owner-dashboard-details", function(req, res) {
 });
 
 app.post("/delete-item", function(req, res) {
-  console.log("Inside addtocart POST");
+  console.log("Inside delete item from menu POST");
   console.log("Request Body: ", req.body);
-  //console.log("Request Body: ", req.body.item_id);
   const userSession = req.session.user;
 
   if (req.session.user) {
@@ -151,27 +150,9 @@ app.post("/delete-item", function(req, res) {
         });
         res.end("Error in establishing connection!");
       } else {
-        // const order_id = Math.floor(Math.random() * 10000);
-        // const quantity = 1;
-        // const item_id = req.body.item_id;
-        // // console.log("Item_ID: ", item_id);
         var sql =
           "DELETE from item WHERE (item_id) = " +
           mysql.escape(req.body.item_id);
-        // var sql =
-        //   "INSERT into cart (item_id,res_id,email,item_name,quantity,price) VALUES(" +
-        //   mysql.escape(req.body.item_id) +
-        //   "," +
-        //   mysql.escape(req.body.res_id) +
-        //   "," +
-        //   mysql.escape(req.session.user.email) +
-        //   "," +
-        //   mysql.escape(req.body.item_name) +
-        //   "," +
-        //   mysql.escape(quantity) +
-        //   "," +
-        //   mysql.escape(req.body.price) +
-        //   ");";
 
         conn.query(sql, function(err, result) {
           if (err) {
@@ -191,6 +172,333 @@ app.post("/delete-item", function(req, res) {
       }
     });
   }
+});
+
+app.get("/breakfast-details", function(req, res) {
+  console.log("Inside Breakfast Details GET!");
+  const userSession = req.session.user;
+
+  pool.getConnection(function(err, conn) {
+    if (err) {
+      console.log("Error in creating connection!");
+      res.writeHead(400, {
+        "Content-type": "text/plain"
+      });
+      res.end("Error in creating connection!");
+    } else {
+      var presql =
+        "SELECT r.res_id from restaurant as r inner join owner as o where r.res_id=o.res_id and o.owner_id=" +
+        mysql.escape(userSession.owner_id);
+
+      conn.query(presql, function(err, result) {
+        if (err) {
+          res.writeHead(400, {
+            "Content-type": "text/plain"
+          });
+          console.log("Error in getting restaurant id");
+          res.end("Error in getting restaurant id");
+        } else {
+          const res_id = result[0].res_id;
+          console.log(res_id);
+          console.log("Got restaurant id successful!");
+          const section = "Dinner";
+          //const item_id = Math.floor(Math.random() * 1000);
+          var sql =
+            'SELECT * from item where section="Breakfast" AND res_id = ' +
+            mysql.escape(res_id);
+          // +
+          // "and section = " +
+          // mysql.escape(section);
+
+          conn.query(sql, function(err, result) {
+            if (err) {
+              // res.writeHead(400, {
+              //   "Content-type": "text/plain"
+              // });
+              console.log("Error in getting  items");
+              res.json({ data: "Error in getting items" });
+            } else {
+              console.log("Items in Dashboard loaded successfully");
+              console.log(result);
+              // res.writeHead(200, {
+              //   "Content-type": "application/json"
+              // });
+              console.log(JSON.stringify(result));
+              //res.end({ data: result });
+
+              res.send(200, JSON.stringify(result));
+            }
+          });
+        }
+      });
+    }
+  });
+});
+
+app.get("/lunch-details", function(req, res) {
+  console.log("Inside Lunch Details GET!");
+  const userSession = req.session.user;
+
+  pool.getConnection(function(err, conn) {
+    if (err) {
+      console.log("Error in creating connection!");
+      res.writeHead(400, {
+        "Content-type": "text/plain"
+      });
+      res.end("Error in creating connection!");
+    } else {
+      var presql =
+        "SELECT r.res_id from restaurant as r inner join owner as o where r.res_id=o.res_id and o.owner_id=" +
+        mysql.escape(userSession.owner_id);
+
+      conn.query(presql, function(err, result) {
+        if (err) {
+          res.writeHead(400, {
+            "Content-type": "text/plain"
+          });
+          console.log("Error in getting restaurant id");
+          res.end("Error in getting restaurant id");
+        } else {
+          const res_id = result[0].res_id;
+          console.log(res_id);
+          console.log("Got restaurant id successful!");
+
+          var sql =
+            'SELECT * from item where section="lunch" AND res_id = ' +
+            mysql.escape(res_id);
+          // +
+          // "and section = " +
+          // mysql.escape(section);
+
+          conn.query(sql, function(err, result) {
+            if (err) {
+              // res.writeHead(400, {
+              //   "Content-type": "text/plain"
+              // });
+              console.log("Error in getting  items");
+              res.json({ data: "Error in getting items" });
+            } else {
+              console.log("Items in Dashboard loaded successfully");
+              console.log(result);
+              // res.writeHead(200, {
+              //   "Content-type": "application/json"
+              // });
+              console.log(JSON.stringify(result));
+              //res.end({ data: result });
+
+              res.send(200, JSON.stringify(result));
+            }
+          });
+        }
+      });
+    }
+  });
+});
+app.get("/dinner-details", function(req, res) {
+  console.log("Inside Dinner Details GET!");
+  const userSession = req.session.user;
+
+  pool.getConnection(function(err, conn) {
+    if (err) {
+      console.log("Error in creating connection!");
+      res.writeHead(400, {
+        "Content-type": "text/plain"
+      });
+      res.end("Error in creating connection!");
+    } else {
+      var presql =
+        "SELECT r.res_id from restaurant as r inner join owner as o where r.res_id=o.res_id and o.owner_id=" +
+        mysql.escape(userSession.owner_id);
+
+      conn.query(presql, function(err, result) {
+        if (err) {
+          res.writeHead(400, {
+            "Content-type": "text/plain"
+          });
+          console.log("Error in getting restaurant id");
+          res.end("Error in getting restaurant id");
+        } else {
+          const res_id = result[0].res_id;
+          console.log(res_id);
+          console.log("Got restaurant id successful!");
+          const section = "Dinner";
+          //const item_id = Math.floor(Math.random() * 1000);
+          var sql =
+            'SELECT * from item where section="dinner" AND res_id = ' +
+            mysql.escape(res_id);
+          // +
+          // "and section = " +
+          // mysql.escape(section);
+
+          conn.query(sql, function(err, result) {
+            if (err) {
+              // res.writeHead(400, {
+              //   "Content-type": "text/plain"
+              // });
+              console.log("Error in getting  items");
+              res.json({ data: "Error in getting items" });
+            } else {
+              console.log("Items in Dashboard loaded successfully");
+              console.log(result);
+              // res.writeHead(200, {
+              //   "Content-type": "application/json"
+              // });
+              console.log(JSON.stringify(result));
+              //res.end({ data: result });
+
+              res.send(200, JSON.stringify(result));
+            }
+          });
+        }
+      });
+    }
+  });
+});
+
+app.post("/update-owner-order", function(req, res) {
+  console.log("Inside Update Order POST");
+  console.log("Request Body:", req.body);
+  console.log("Res_id:", req.body[0].res_id);
+  console.log("Req Length: ", req.body.length);
+
+  const res_id = req.body[0].res_id;
+  const email = req.body[0].email;
+  // const order_id = 101;
+  // const res_id = 1;
+  // const email = "nidhi@gmail.com";
+  pool.getConnection(function(err, conn) {
+    if (err) {
+      console.log("Error in establishing connection!");
+      res.writeHead(400, {
+        "Content-type": "text/plain"
+      });
+      res.end("Error in establishing connection!");
+    } else {
+      /*var sql2 =
+        "UPDATE orders (status) VALUES(" +
+        mysql.escape(status) +
+        ");";
+
+      conn.query(sql2, function(err, result) {
+        if (err) {
+          console.log("Error in adding order");
+          res.writeHead(400, {
+            "Content-type": "text/plain"
+          });
+          res.end("Error in adding an order");
+        } else {
+          console.log("Adding order successful!");
+          // res.writeHead(200, {
+          //   "Content-type": "text/plain"
+          // });
+          res.end("Adding order successful!");
+        }
+      });*/
+
+      for (var index = 0; index < req.body.length; index++) {
+        var sql3 =
+          "UPDATE orders set " +
+          "status = " +
+          mysql.escape(req.body[index].status) +
+          " WHERE order_id = " +
+          mysql.escape(req.body[index].order_id);
+
+        conn.query(sql3, function(err, result) {
+          if (err) {
+            console.log("Error in updating order");
+          } else {
+            console.log("Order updated successfully!", index);
+            res.end("Order updated successfully!");
+          }
+        });
+      }
+    }
+  });
+});
+
+app.post("/update-item", function(req, res) {
+  console.log("Inside UPDATE ITEM POST");
+  console.log("Request Body:", req.body);
+  const userSession = req.session.user;
+
+  if (req.session.user) {
+    pool.getConnection(function(err, conn) {
+      if (err) {
+        console.log("Error in creating connection");
+        res.writeHead(400, {
+          "Content-type": "text/plain"
+        });
+        res.end("Error in creating connection!");
+      } else {
+        var sql =
+          "UPDATE item set" +
+          "item_name=" +
+          mysql.escape(req.body.item_name) +
+          "," +
+          "description=" +
+          mysql.escape(req.body.description) +
+          "," +
+          "price=" +
+          mysql.escape(req.body.price) +
+          "," +
+          "section=" +
+          mysql.escape(req.body.section) +
+          "WHERE item_id=" +
+          mysql.escape(825);
+
+        conn.query(sql, function(err, result) {
+          if (err) {
+            console.log("Error in updating items");
+            res.writeHead(400, {
+              "Content-type": "teext/plain"
+            });
+            res.end("Error in updating items");
+          } else {
+            console.log("Update Success");
+            result.writeHead(200, {
+              "Content-type": "application.json"
+            });
+            res.end("Updated!");
+          }
+        });
+      }
+    });
+  }
+});
+app.get("/owner-order-details", function(req, res) {
+  console.log("Inside Owner Order Details GET!");
+  const userSession = req.session.user;
+  console.log(req.session.user);
+
+  pool.getConnection(function(err, conn) {
+    if (err) {
+      console.log("Error in creating connection!");
+      res.writeHead(400, {
+        "Content-type": "text/plain"
+      });
+      res.end("Error in creating connection!");
+    } else {
+      console.log("Established connection");
+      var sql =
+        "SELECT * from orders as o where o.res_id= (SELECT ow.res_id from owner as ow where ow.owner_id=" +
+        mysql.escape(userSession.owner_id) +
+        ");";
+
+      conn.query(sql, function(err, result) {
+        if (err) {
+          console.log("Error in getting order details");
+
+          res.json({ data: "Error in getting order details" });
+        } else {
+          console.log("Orders loaded successfully");
+          console.log(result);
+          console.log(JSON.stringify(result));
+
+          res.send(JSON.stringify(result));
+        }
+      });
+    }
+  });
 });
 
 module.exports = app;
