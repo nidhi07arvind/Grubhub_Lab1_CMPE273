84c341fbe31301bb85c606fe4fd854c4804bd580
nidhi07arvind <49506508+nidhi07arvind@users.noreply.github.com>
2019-10-06 21:53:47 -0700

    Owner views

--- frontend/src/components/OwnerDashboard/OwnerDashboard.js
+++ frontend/src/components/OwnerDashboard/OwnerDashboard.js
@@ -3,6 +3,8 @@ import Header from "../Header/Header";
 import cookie from "react-cookies";
 import { Redirect } from "react-router";
 import axios from "axios";
+import { Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.css";
 
 class OwnerDashboard extends Component {
   constructor() {
@@ -10,8 +12,12 @@ class OwnerDashboard extends Component {
     this.state = {
       items: [],
       itemDetails: [],
-      errorRedirect: false
+      errorRedirect: false,
+      isLunch: false
     };
+    this.saveChanges = this.saveChanges.bind(this);
+    //this.handleChange = this.handleChange.bind(this);
+    //this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   componentWillMount() {
@@ -26,38 +32,37 @@ class OwnerDashboard extends Component {
             items: response.data
           });
 
-          var itemDetails = [];
-          for (let i = 0; i < this.state.items.length; i++) {
-            var data = {
-              ItemId: this.state.items[i].item_id
-            };
+          // var itemDetails = [];
+          // for (let i = 0; i < this.state.items.length; i++) {
+          //   var data = {
+          //     item_id: this.state.items[i].item_id
+          //   };
 
-            axios
-              .post("http://localhost:3001/item-details", data)
-              .then(response => {
-                if (response.status === 200) {
-                  var itemDetail = response.data;
-                  itemDetail["item_name"] = this.state.items[i].item_name;
-                  itemDetail["description"] = this.state.items[i].description;
-                  itemDetail["image"] = this.state.items[i].image;
-                  itemDetail["price"] = this.state.items[i].price;
-                  itemDetail["section"] = this.state.items[i].section;
-
-                  itemDetails.push(itemDetail);
-                  this.setState({
-                    itemDetails: itemDetails
-                  });
-                  console.log("Item Details: ", this.state.itemDetails);
-                }
-              })
-              .catch(err => {
-                if (err) {
-                  this.setState({
-                    errorRedirect: true
-                  });
-                }
-              });
-          }
+          // axios
+          //   .post("http://localhost:3001/item-details", data)
+          //   .then(response => {
+          //     if (response.status === 200) {
+          //       var itemDetail = response.data;
+          //       itemDetail["item_id"] = this.state.items[i].item_id;
+          //       itemDetail["res_id"] = this.state.items[i].res_id;
+          //       itemDetail["item_name"] = this.state.items[i].item_name;
+          //       itemDetail["description"] = this.state.items[i].description;
+          //       itemDetail["price"] = this.state.items[i].price;
+          //       itemDetails.push(itemDetail);
+          //       this.setState({
+          //         itemDetails: itemDetails
+          //       });
+          //       console.log("Item Details: ", this.state.itemDetails);
+          //     }
+          //   })
+          // .catch(err => {
+          //   if (err) {
+          //     this.setState({
+          //       errorRedirect: true
+          //     });
+          //   }
+          // });
+          //}
         }
       })
       .catch(err => {
@@ -69,40 +74,72 @@ class OwnerDashboard extends Component {
       });
   }
 
-  render() {
-    let redirectVar = null;
-    /*if (!cookie.load('cookie')) {
-            redrirectVar = <Redirect to="/login" />
-        }
-        if (this.state.errorRedirect === true) {
-            redrirectVar = <Redirect to="/error" />
-        }*/
+  saveChanges = item => e => {
+    console.log(item);
 
-    let itemDetails = this.state.itemDetails.map(function(item, index) {
+    const data = item;
+
+    axios.defaults.withCredentials = true;
+
+    axios
+      .post("http://localhost:3001/delete-item", data)
+      .then(response => {
+        if (response.status === 200) {
+          console.log(response.data);
+        }
+      })
+      .catch(err => {
+        if (err) {
+          this.setState({
+            errorRedirect: true
+          });
+        }
+      });
+  };
+
+  render() {
+    let redrirectVar = null;
+    if (!cookie.load("cookie")) {
+      redrirectVar = <Redirect to="/login" />;
+    }
+    if (this.state.errorRedirect === true) {
+      redrirectVar = <Redirect to="/error" />;
+    }
+    console.log(this.state.items);
+    let itemDetails = this.state.items.map(function(item, index) {
       return (
         <div className="container trip-details-container" key={index}>
+          <Link to={"/update-item/" + item.item_id}>Update Item</Link>
           <div className="trip-details-content border">
             <div className="trip-main-details blue-text">
-              <h2>
-                <strong>{item.Headline}</strong>
-              </h2>
-              <div>Item Section Type : {item.section}</div>
-              <div>Name: {item.name}</div>
+              <div>Item ID: {item.item_id}</div>
+              <div>Item Name: {item.item_name}</div>
+              <div>Price: {item.price}</div>
               <div>Description: {item.description}</div>
-              <div>Image: {item.image}</div>
-              <div>Price: {item.price} guests</div>
+              <br></br>
+              <button
+                className="btn btn-danger"
+                onClick={this.saveChanges(item)}
+              >
+                Delete Item
+              </button>
             </div>
           </div>
         </div>
       );
-    });
+    }, this);
 
     return (
       <div>
+        <Header />
         <div className="dashboard-container">
           <div className="center-content owner-dashboard-banner">
             <h1>Dashboard</h1>
-            <h1>Items are:</h1>
+            <span>
+              <Link to={"/breakfast-display"}>Breakfast</Link>{" "}
+              <Link to={"/lunch-display"}>Lunch</Link>{" "}
+              <Link to={"/diner-display"}>Dinner</Link>
+            </span>
           </div>
           <div>{itemDetails}</div>
         </div>
