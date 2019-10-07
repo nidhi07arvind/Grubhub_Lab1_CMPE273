d36835a9202b8829ba7a9b91b29b8b32772f8284
nidhi07arvind <49506508+nidhi07arvind@users.noreply.github.com>
2019-10-06 21:44:59 -0700

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 import Main from "./components/Main";
 import { BrowserRouter } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.css";
 
 class App extends Component {
   render() {
